72619b71fe857d8b6f0486b31638ede096700c27
Isabelle <isabelle.carlsson@my.jcu.edu.au>
2019-04-10 13:51:38 +1000

    Add correct style to fragments

--- app/src/main/java/com/example/guesstheceleb/MainActivity.java
+++ app/src/main/java/com/example/guesstheceleb/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.guesstheceleb;
 
+import android.content.res.AssetManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
+    ImageManager imageManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
